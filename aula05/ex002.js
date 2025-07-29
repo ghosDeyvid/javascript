62bc2d1ba1e38d9bc4006c6f7a7a3423efb4531a
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-07-29 10:10:04 -0300

    l

---
+++ aula05/ex002.js
@@ -0,0 +1,7 @@
+var país ='Fraces'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você e Brasileiro')
+}else{
+    console.log('Você é Estrageiro')
+}
---
+++ aula05/ex008.js
@@ -0,0 +1,6 @@
+let vel = 78
+console.log(`A velocidade de seu carro e ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você foi MUTADO por passar aacima da velocidade permitida`)
+}
+console.log( `dirija com cuidadeo`)
