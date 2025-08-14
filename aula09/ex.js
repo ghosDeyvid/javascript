1e2daa54c3229eefabe60c7d0c052b78ab2cec98
deyvid siqueira miguel <deyvidmycny@gmail.com>
2025-08-14 09:54:42 -0300

    ffd

--- aula09/ex.js
+++ aula09/ex.js
@@ -1,6 +1,25 @@
+let c =1
+do{
+    console.log( `passo${c}`)
+    c++
+}while(c<=500)
+/*
+let c =1
+while(c<=500){
+    console.log( `passo${c}`)
+    c++
+}
+    */
+
+
+
+
+
+
+
+/*console.log('tudo bem ')
 console.log('tudo bem ')
 console.log('tudo bem ')
 console.log('tudo bem ')
 console.log('tudo bem ')
-console.log('tudo bem ')
-console.log('tudo bem ')
+console.log('tudo bem ')*/
---
+++ aula09/ex002.js
@@ -0,0 +1,14 @@
+/*
+let c = 1
+while(c<=5){
+    console.log(c)
+    c++
+}
+*/
+
+//-----
+console.log('Vai comesar...')
+ for( var c = 1 ; c<=5; c++ ) {
+    console.log(c)
+ } 
+ console.log('fim')
